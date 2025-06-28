0a1646e9067bbc3448d574f9bd01e4821f8a0435
Shmuye <shmuye27@gmail.com>
2025-06-28 06:44:47 +0300

    import correction

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
-import * as THREE from "https://unpkg.com/three@0.158.0/build/three.module.js"
-import { OrbitControls } from "https://unpkg.com/three@0.158.0/examples/jsm/controls/OrbitControls.js"
+import * as THREE from 'three'
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js"
 import { Pendulum } from "./pendulum.js"
 import { WaveSimulation } from "./wave.js"
 import { ControlsManager } from "./controls.js"
--- js/pendulum.js
+++ js/pendulum.js
@@ -1,4 +1,4 @@
-import * as THREE from "https://unpkg.com/three@0.158.0/build/three.module.js"
+import * as THREE from "three"
 
 /**
  * Pendulum class - represents a single pendulum with realistic physics
